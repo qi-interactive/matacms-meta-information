aecf98b41959c00c066b644737c83ff7bbe00de1
Michal Fiedorowicz <michal@qi-interactive.com>
2016-10-27 13:38:49 +0100

    Added MetaInformation helper

---
+++ helpers/MetaInformation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace matacms\metainformation\helpers;
+
+/**
+ * Description of MetaInformation
+ *
+ * @author mfiedorowicz
+ */
+class MetaInformation {
+
+    public static function register($metaInformationModel) {
+
+        $view = \Yii::$app->controller->view;
+
+        if(!empty($metaInformationModel->Keywords)) {
+            $view->registerMetaTag([
+                "property" => "keywords",
+                "content" => $metaInformationModel->Keywords,
+                ]);
+        }
+
+        if(!empty($metaInformationModel->Description)) {
+            $view->registerMetaTag([
+                "property" => "description",
+                "content" => $metaInformationModel->Description,
+                ]);
+        }
+    }
+
+}
